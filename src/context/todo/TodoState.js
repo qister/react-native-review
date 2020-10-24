27428e9a7794cf73ef4c73ea527fffafa1642cd0
qister <qistersick@gmail.com>
2020-10-24 15:22:57 +0300

    Добавлена обработка ошибок

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -7,7 +7,7 @@ import {
   FETCH_TODOS,
   HIDE_LOADER,
   REMOVE_TODO,
-  sHOW_ERROR,
+  SHOW_ERROR,
   SHOW_LOADER,
   UPDATE_TODO,
 } from '../types'
@@ -63,35 +63,43 @@ export const TodoState = ({ children }) => {
     )
   }
 
-  const fetchTodos = async () => {
-    showLoader()
-    const response = await fetch(
-      'https://react-native-todo-app-5db5e.firebaseio.com/todos.json',
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    )
-
-    const data = await response.json()
-    // console.log('Fetch Data: ', data)
-    const todos = Object.keys(data).map(key => ({...data[key], id: key}))
-    dispatch({type: FETCH_TODOS, todos})
-    hideLoader()
-    
-  }
-
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
 
   const showLoader = () => dispatch({ type: SHOW_LOADER })
 
   const hideLoader = () => dispatch({ type: HIDE_LOADER })
 
-  const showError = (error) => dispatch({ type: sHOW_ERROR, error })
+  const showError = (error) => dispatch({ type: SHOW_ERROR, error })
 
   const clearError = () => dispatch({ type: CLEAR_ERROR })
+
+  const fetchTodos = async () => {
+    showLoader()
+    clearError()
+    try {
+      const response = await fetch(
+        'https://react-native-todo-app-5db5e.firebaseio.com/todos.json',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+
+      const data = await response.json()
+      // console.log('Fetch Data: ', data)
+      const todos = Object.keys(data).map((key) => ({ ...data[key], id: key }))
+      dispatch({ type: FETCH_TODOS, todos })
+    } catch (e) {
+      showError('Что-то пошло не так, попробуйте снова')
+      console.log(e)
+    } finally {
+      hideLoader()
+    }
+  }
+
+
 
   return (
     <TodoContext.Provider
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -2,20 +2,24 @@ import React, { useState, useEffect, useContext, useCallback } from 'react'
 import { StyleSheet, View, FlatList, Image, Dimensions } from 'react-native'
 import { AddTodo } from '../components/AddTodo'
 import { Todo } from '../components/Todo'
+import { AppButton } from '../components/ui/AppButton'
 import { AppLoader } from '../components/ui/AppLoader'
+import { AppText } from '../components/ui/AppText'
 import { ScreenContext } from '../context/screen/screenContext'
 import { TodoContext } from '../context/todo/todoContext'
 import { THEME } from '../theme'
 
 export const MainScreen = () => {
-  const { addTodo, todos, removeTodo, fetchTodos, loading, error } = useContext(TodoContext)
+  const { addTodo, todos, removeTodo, fetchTodos, loading, error } = useContext(
+    TodoContext,
+  )
   const { changeScreen } = useContext(ScreenContext)
 
   const [deviceWidth, setDeviceWidth] = useState(
     Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2,
   )
 
-  const loadTodos = useCallback( async () => await fetchTodos(), [fetchTodos])
+  const loadTodos = useCallback(async () => await fetchTodos(), [fetchTodos])
 
   useEffect(() => {
     loadTodos()
@@ -35,7 +39,18 @@ export const MainScreen = () => {
   })
 
   if (loading) {
-    return <AppLoader/>
+    return <AppLoader />
+  }
+
+  console.log('Error: ', error)
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <AppText style={styles.error}>{error}</AppText>
+        <AppButton onPress={loadTodos}>Повторить</AppButton>
+      </View>
+    )
   }
 
   let content = (
@@ -88,5 +103,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     resizeMode: 'contain',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  error: {
+    fontSize: 20,
+    color: THEME.DANGER_COLOR,
   },
 })
